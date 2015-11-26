38d8f34ecfd45b1e01256b297cc7b3f4eaa06fdd
Hatidzhe <yusufova.h@gmail.com>
2015-11-26 07:49:31 +0200

    chg: next step of developing

--- src/com/hatidzhe/Main.java
+++ src/com/hatidzhe/Main.java
@@ -6,16 +6,20 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Die dice = new Die();
-        dice.rolls();
-        System.out.println("The die comes up " + dice.getDie1());
+        Die d = new Die();
+        d.rolls();
+        System.out.println("The die comes up " + d.getDie1());
+
 
         DiceThrow die1 = new DiceThrow();
         DiceThrow die2 = new DiceThrow();
         DiceThrow die3 = new DiceThrow();
+
+
         die1.roll();
         die2.roll();
         die3.roll();
+
         System.out.println("The dice come up " + die1.getDie1() + " " + die2.getDie2() + " " + die3.getDie3());
     }
 
