3a44af5671033597b1a7692b271e551b701337ec
Hatidzhe <yusufova.h@gmail.com>
2015-11-26 07:52:56 +0200

    chg: concerns separated

--- src/com/hatidzhe/DiceThrow.java
+++ src/com/hatidzhe/DiceThrow.java
@@ -3,34 +3,41 @@ package com.hatidzhe;
 
 public class DiceThrow {
 
-    public int die1, die2, die3;
+    int die1, die2, die3;
 
-    public void roll() {
-
-        // a random number between 1 and 6
-        die1 = (int) (Math.random() * 6) + 1;
-        die2 = (int) (Math.random() * 6) + 1;
-        die3 = (int) (Math.random() * 6) + 1;
+    public DiceThrow() {
+        roll();
 
     }
 
+
+    public void roll() {
+
+        Die die = new Die();
+
+        die1 = die.getTotal();
+        die2 = die.getTotal();
+        die3 = die.getTotal();
+
+
+    }
+
+
     public int getDie1() {
+
+
         // Return the number showing on the first die.
         return die1;
     }
 
     public int getDie2() {
-        // Return the number showing on the first die.
+
         return die2;
     }
 
     public int getDie3() {
-        // Return the number showing on the first die.
+
         return die3;
     }
 
-    public int getTotal() {
-        // Return the total showing on the two dice.
-        return die1 + die2 + die3;
-    }
 }
