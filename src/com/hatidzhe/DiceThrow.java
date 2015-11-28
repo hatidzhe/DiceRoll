ff2719f725fc9f3650fd60a5f763ec86dd719be9
Hatidzhe <yusufova.h@gmail.com>
2015-11-28 20:05:35 +0200

    chg: last stand

--- src/com/hatidzhe/DiceThrow.java
+++ src/com/hatidzhe/DiceThrow.java
@@ -3,28 +3,33 @@ package com.hatidzhe;
 
 public class DiceThrow {
 
-    int die1, die2, die3;
+    public int die1, die2, die3;
+
 
     public DiceThrow() {
-        roll();
 
+        roll();
     }
 
 
     public void roll() {
 
-        Die die = new Die();
+        Die d = new Die();
+        Die dd = new Die();
+        Die ddd = new Die();
 
-        die1 = die.getTotal();
-        die2 = die.getTotal();
-        die3 = die.getTotal();
+        d.rolls();
+        dd.rolls();
+        ddd.rolls();
 
+        die1 = d.getDie1();
+        die2 = dd.getDie1();
+        die3 = ddd.getDie1();
 
     }
 
 
     public int getDie1() {
-
 
         // Return the number showing on the first die.
         return die1;
@@ -32,12 +37,15 @@ public class DiceThrow {
 
     public int getDie2() {
 
+
         return die2;
     }
 
     public int getDie3() {
 
+
         return die3;
     }
+
 
 }
