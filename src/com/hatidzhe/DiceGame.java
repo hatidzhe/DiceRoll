0a144722db1f7f702fdb82ab98f9f4a170c99d29
Hatidzhe <yusufova.h@gmail.com>
2015-11-29 18:45:32 +0200

    chg: adjusted

--- src/com/hatidzhe/DiceGame.java
+++ src/com/hatidzhe/DiceGame.java
@@ -3,7 +3,6 @@ package com.hatidzhe;
 
 public class DiceGame {
 
-    public int die1, die2, die3;
 
     public DiceGame() {
         play();
@@ -12,41 +11,11 @@ public class DiceGame {
     public void play() {
 
         DiceThrow play1 = new DiceThrow();
-        DiceThrow play2 = new DiceThrow();
-        DiceThrow play3 = new DiceThrow();
 
         play1.roll();
-
-        die1 = play1.getDie1();
-        die2 = play1.getDie2();
-        die3 = play1.getDie3();
-
-        die1 = play2.getDie1();
-        die2 = play2.getDie2();
-        die3 = play2.getDie3();
-
-        die1 = play3.getDie1();
-        die2 = play3.getDie2();
-        die3 = play3.getDie3();
+        System.out.println("The dice game results with " + play1.getResult());
 
     }
 
-    public int getDie1() {
-
-        // Return the number showing on the first die.
-        return die1;
-    }
-
-    public int getDie2() {
-
-
-        return die2;
-    }
-
-    public int getDie3() {
-
-
-        return die3;
-    }
 
 }
